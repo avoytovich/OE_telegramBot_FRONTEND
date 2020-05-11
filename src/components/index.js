a3ff884567967e8c97062e468eac97fe9dccbd78
avoytovich <levanwork@gmail.com>
2020-05-11 22:19:29 +0300

    add: bookmark

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,8 @@ import Head from './Header';
 import Dashboard from './Dashboard';
 import Group from './Group';
 import SubGroup from './SubGroup';
+import Bookmark from './Bookmark';
 import LandingPage from './LandingPage';
 import Test from './Test';
 
-export { Head, Dashboard, Group, SubGroup, LandingPage, Test };
+export { Head, Dashboard, Group, SubGroup, Bookmark, LandingPage, Test };
