48a1f0fc147f2b93d42cf23d32e8103f19524942
avoytovich <levanwork@gmail.com>
2020-05-21 00:15:23 +0300

    add: sign-up

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Grid, Link, Typography } from '@material-ui/core';
 import { withRouter } from 'react-router-dom';
-import { get } from 'lodash';
 
 import checkAuth from './../../helper/redirections';
 import SVG from './../../helper/customizeIcon';
@@ -12,49 +11,12 @@ import imageAvatar from './../../assets/images/avatar.svg';
 import './header.sass';
 
 function Head(props) {
-  const links = {
-    signUp: {
-      title: 'Sign Up',
-      route: '/bookmark/sign-up',
-    },
-    logOut: {
-      title: 'Log Out',
-      route: '/bookmark',
-    },
+  const logOut = {
+    title: 'Log Out',
+    route: '/bookmark',
   };
 
   const handleLogOut = () => localStorage.setItem('login', null);
-
-  const resolveLink = () => (checkAuth() ? links.signUp : links.logOut);
-
-  const history = get(props, 'history');
-
-  const [title, setTitle] = useState('Sign Up');
-  const [route, setRoute] = useState('/bookmark/sign-up');
-  const [login, setLogin] = useState(
-    get(JSON.parse(localStorage.getItem('login')), 'token')
-  );
-
-  useEffect(() => {
-    if (login) {
-      const { title, route } = resolveLink();
-      setTitle(title);
-      setRoute(route);
-    }
-  });
-
-  const resolveOnClickLink = () => {
-    switch (title) {
-      case 'Log Out':
-        handleLogOut();
-        break;
-      case 'Sign Up':
-        history.push(route);
-        break;
-      default:
-        handleLogOut();
-    }
-  };
 
   //console.log('props Header', props);
   return (
@@ -74,24 +36,24 @@ function Head(props) {
               <Typography className="info-title">Bookmark's</Typography>
             </Grid>
           </Grid>
-          <Grid item xs={2} sm={2} className="container-link">
-            {!checkAuth() && (
+          {!checkAuth() && (
+            <Grid item xs={2} sm={2} className="container-link">
               <SVG
                 className="link-avatar"
                 width="48px"
                 height="48px"
                 source={imageAvatar}
               />
-            )}
-            <Link
-              href={route}
-              className="link-auth"
-              onClick={resolveOnClickLink}
-              style={{ color: '#470b2f' }}
-            >
-              <Typography className="link-title">{title}</Typography>
-            </Link>
-          </Grid>
+              <Link
+                href={logOut.route}
+                className="link-auth"
+                onClick={handleLogOut}
+                style={{ color: '#470b2f' }}
+              >
+                <Typography className="link-title">{logOut.title}</Typography>
+              </Link>
+            </Grid>
+          )}
         </Grid>
       </Grid>
     </div>
