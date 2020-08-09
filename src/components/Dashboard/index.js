b74b57350660fb438353da562b70b14ff26acf55
avoytovich <levanwork@gmail.com>
2020-08-09 14:34:07 +0300

    add: invitation mail

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect, useCallback, Fragment } from 'react';
 import { withRouter } from 'react-router-dom';
-import { Grid, Typography } from '@material-ui/core';
+import { Grid, Typography, TextField, Button } from '@material-ui/core';
 import 'antd/dist/antd.css';
-import { Button, Table } from 'antd';
+import { Table } from 'antd';
 import { get } from 'lodash';
 
 import Head from './../Header';
@@ -12,13 +12,14 @@ import { API } from './../../helper/constants';
 import { wrapRequest } from './../../utils/api';
 
 import './dashboard.sass';
+import invitation from './../../utils/mails/templates/invitation.html';
 
 function Dashboard(props) {
   // console.log('Dashboard props', props);
 
   const [isSending, setIsSending] = useState(false);
   const [execFetch, setExecFetch] = useState(false);
-  const [delFollowers, setDelFollowers] = useState([]);
+  const [selectedFollowers, setSelectedFollowers] = useState([]);
 
   const user_id = get(props, 'match.params.id');
 
@@ -94,8 +95,10 @@ function Dashboard(props) {
 
   const rowSelection = {
     onChange: (selectedRowKeys, selectedRows) => {
-      const preDelFollowers = selectedRows.filter(each => each !== undefined);
-      setDelFollowers(preDelFollowers);
+      const presSelectedFollowers = selectedRows.filter(
+        each => each !== undefined
+      );
+      setSelectedFollowers(presSelectedFollowers);
       // console.log(
       //   `selectedRowKeys: ${selectedRowKeys}`,
       //   'selectedRows: ',
@@ -108,10 +111,10 @@ function Dashboard(props) {
     }),
   };
 
-  const modalContent = modal => (
+  const modalContentDelete = modal => (
     <Fragment>
       <Typography className="modal-question">
-        {`${delFollowers.length} followers
+        {`${selectedFollowers.length} followers
           will be deleted at all! Are you sure?`}
       </Typography>
       <div className="modal-nav">
@@ -140,7 +143,7 @@ function Dashboard(props) {
   );
 
   const confirmationOnDelete = () => {
-    props.setModalContent(modalContent(props.modal));
+    props.setModalContent(modalContentDelete(props.modal));
     props.modal.current.open();
   };
 
@@ -150,7 +153,7 @@ function Dashboard(props) {
     await wrapRequest({
       method: 'DELETE',
       url: `${API.URL[process.env.NODE_ENV]}/user/${user_id}/followers_delete`,
-      data: delFollowers,
+      data: selectedFollowers,
       mode: 'cors',
       cache: 'default',
     })
@@ -160,7 +163,136 @@ function Dashboard(props) {
       })
       .catch(e => props.dispatchErrorNotifiction('errorNotification', e));
     setIsSending(false);
-  }, [delFollowers]);
+  }, [selectedFollowers]);
+
+  const inputFields = [
+    {
+      label: 'Join Zoom Meeting',
+      type: 'url',
+      placeholder: '',
+    },
+    {
+      label: 'Meeting ID',
+      type: 'number',
+      placeholder: '',
+    },
+    {
+      label: 'Passcode',
+      type: 'text',
+      placeholder: '',
+    },
+    {
+      label: 'video',
+      type: 'url',
+      placeholder: '',
+    },
+  ];
+
+  let meetingLink, meetingId, meetingPasscode, videoLink;
+
+  const handleChange = (value, label) => {
+    switch (label) {
+      case 'Join Zoom Meeting':
+        meetingLink = value;
+        break;
+      case 'Meeting ID':
+        meetingId = value;
+        break;
+      case 'Passcode':
+        meetingPasscode = value;
+        break;
+      case 'video':
+        videoLink = value;
+        break;
+    }
+  };
+
+  const sendInitialMail = useCallback(
+    async event => {
+      event.preventDefault();
+      if (isSending) return;
+      setIsSending(true);
+      const newInvitation = invitation
+        .replace('#meetingGreeting', `Hi, ${selectedFollowers[0].first_name}!`)
+        .replace(
+          '#meetingTime',
+          `${selectedFollowers[0].time} ${selectedFollowers[0].date}`
+        )
+        .replace('#meetingLink', `${meetingLink}`)
+        .replace('#meetingId', `${meetingId}`)
+        .replace('#meetingPasscode', `${meetingPasscode}`)
+        .replace('#videoLink', `${videoLink}`);
+      await wrapRequest({
+        method: 'POST',
+        url: `${API.URL[process.env.NODE_ENV]}/user/${user_id}/mail_send`,
+        data: {
+          payload: newInvitation,
+          email: selectedFollowers[0].email,
+        },
+        mode: 'cors',
+        cache: 'default',
+      })
+        .then(data => props.modal.current.close())
+        .catch(e => props.dispatchErrorNotifiction('errorNotification', e));
+      setIsSending(false);
+    },
+    [selectedFollowers]
+  );
+
+  const modalContentInvite = modal => (
+    <Fragment>
+      <form onSubmit={sendInitialMail}>
+        <div className="modal-invitation">
+          {inputFields.map((each, id) => (
+            <TextField
+              key={id}
+              id={each.label}
+              name={each.label}
+              label={each.label.toUpperCase()}
+              placeholder={each.placeholder}
+              inputProps={{
+                type: each.type,
+              }}
+              onChange={e => handleChange(e.target.value, each.label)}
+              style={{
+                borderRadius: '5px',
+                backgroundColor: '#e4e4e4',
+                marginBottom: '5px',
+              }}
+              fullWidth
+            />
+          ))}
+        </div>
+        <div className="modal-nav">
+          <Button
+            type="submit"
+            color="primary"
+            style={{
+              border: 'green',
+              backgroundColor: 'green',
+            }}
+          >
+            SEND
+          </Button>
+          <Button
+            type="primary"
+            style={{
+              border: 'red',
+              backgroundColor: 'red',
+            }}
+            onClick={modal.current.close}
+          >
+            CANCEL
+          </Button>
+        </div>
+      </form>
+    </Fragment>
+  );
+
+  const confirmationOnInvite = () => {
+    props.setModalContent(modalContentInvite(props.modal));
+    props.modal.current.open();
+  };
 
   return (
     <div className="wrapper-dashboard">
@@ -185,16 +317,30 @@ function Dashboard(props) {
                     )}
                   </div>
                   <div className="follower-nav">
-                    <Button
-                      type="primary"
-                      style={{
-                        border: 'red',
-                        backgroundColor: 'red',
-                      }}
-                      onClick={confirmationOnDelete}
-                    >
-                      Delete
-                    </Button>
+                    {selectedFollowers.length == 1 ? (
+                      <Button
+                        type="primary"
+                        style={{
+                          border: 'green',
+                          backgroundColor: 'green',
+                        }}
+                        onClick={confirmationOnInvite}
+                      >
+                        Invitation
+                      </Button>
+                    ) : null}
+                    {selectedFollowers.length >= 1 ? (
+                      <Button
+                        type="primary"
+                        style={{
+                          border: 'red',
+                          backgroundColor: 'red',
+                        }}
+                        onClick={confirmationOnDelete}
+                      >
+                        Delete
+                      </Button>
+                    ) : null}
                   </div>
                 </div>
               </Grid>
